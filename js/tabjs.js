8b33d9c0f5be67ec61ad6d64f1f673a5f7a28147
ichbinn54@gmail.com <ichbinn54@gmail.com>
2019-11-29 21:33:40 +0200

    CSS ,MEDIA ir td.

---
+++ js/tabjs.js
@@ -0,0 +1,40 @@
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+/*
+function sleptiSheetus(){
+    for(let i =0; i < sheetai.lenght;i++){
+        let sheet = sheetai[i];
+        sheet.style.display = 'none';
+    }
+}
+
+function rodytiSheeta(event){
+    let id =event.element.data.sheet;
+    document.getElementById(id).style.display='block';
+}
+
+for(let i =0;i<tabai.lenght;i++){
+    let tab = tabai[i];
+    tab.addEventListener('click',rodytiSheeta);
+}*/
