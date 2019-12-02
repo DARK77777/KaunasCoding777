32346f26048e36cc64a00d51f5553647f28d6d85
ichbinn54@gmail.com <ichbinn54@gmail.com>
2019-12-02 21:34:48 +0200

    JAVASCRIPT,JQUERY

---
+++ js/copyloader.js
@@ -0,0 +1,28 @@
+function load(text) {
+    let meniu = JSON.parse(text);
+    let meniuString = '';
+    for(let i =0;i <meniu.length;i++){
+        let meniuItem = meniu[i];
+        meniuString += '<p>'+ "Vardas: "  + meniuItem.vardas + " Pavarde: " + meniuItem.pavarde + " Mokosi: " + meniuItem.kurmokosi + " Kursas: " + meniuItem.kursas +" Pazymiu masyvas: [" + meniuItem.pazymiuMasyvas + "]" +'</p>';
+    }
+    document.getElementById('isvestiCia').innerHTML = meniuString;
+}
+
+
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'duomenisjs.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+
+        }
+    };
+    xobj.send(null);
+}
+
+loadJSON(load);
